ecfb163abca48514aeb79e043e6c6c2094a209ab
Yogesh Poul <yogeshpoul9999@gmail.com>
2024-05-09 12:50:54 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,45 @@
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+
+// Create an Express app
 const express = require("express");
 const app = express();
-const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
 app.use(cors());
 
-const server = http.createServer(app);
+// Create a HTTP server
+const server = createServer(app);
 
+// Initialize Socket.IO
 const io = new Server(server, {
-  cors: {},
+  cors: {
+    origin: "*",
+  },
 });
 
+// Socket.IO connection event
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  // Event listener for joining a room
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
+  // Event listener for sending a message
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
+  // Event listener for disconnection
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
-});
+// Export the server for Vercel deployment
+module.exports = (req, res) => {
+  // Handle incoming HTTP requests
+  app(req, res);
+};
